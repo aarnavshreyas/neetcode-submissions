94ebe2addd8f6d2e97be4ee8aaf83edb85eeff2c
aarnavshreyas <157312193+aarnavshreyas@users.noreply.github.com>
2026-08-20 11:47:41 +0530

    Bulk sync: 20 submissions

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        unordered_map<char,char>mp{
+            {')','('},
+            {'}','{'},
+            {']','['}
+        };
+
+        stack<char>st;
+
+        for(char c : s){
+            if(c=='(' || c=='{' || c=='['){
+                st.push(c);
+            }else{
+                if(st.empty() || st.top()!=mp[c])
+                    return false;
+
+                    st.pop();
+                
+            }
+        }
+
+        return st.empty();
+    }
+};
